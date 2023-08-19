4c82630c3edc257116fe90e98903bd51ed2276d7
AdriaanConijn <github@patatje.eu>
2023-08-19 14:27:25 +0200

    fix arch case

--- setarch.sh
+++ setarch.sh
@@ -2,7 +2,7 @@
 architecture=""
     case $(uname -m) in
         x86_64) architecture="amd64" ;;
-        arm)    apk --print-arch | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+        arm*)    apk --print-arch | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
     esac
 
 echo $architecture > /tmp/arch
